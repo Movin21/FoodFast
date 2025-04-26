6fb7aa9f83a46406e66fd6f271f6bff32d0ed76b
Movindu Liyanage <142730691+Movin21@users.noreply.github.com>
2025-04-26 22:21:55 +0530

    Update customerAuthController.js

--- auth-services/controllers/customerAuthController.js
+++ auth-services/controllers/customerAuthController.js
@@ -1,5 +1,5 @@
 const Customer = require("../models/Customer");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // Register new customer
